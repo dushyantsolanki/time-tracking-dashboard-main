f7840ce3806161a4ed50d1e87c754f9f1cd02369
dushyantsolanki <dushyantsolanki.2002@gmail.com>
2022-12-18 01:22:15 +0530

    JavaScript Button Logic

---
+++ script.js
@@ -0,0 +1,16 @@
+let b = document.getElementsByTagName("a")[0];
+
+b.addEventListener("click", (e) => {
+
+    // color set when we click
+
+  b.style.color = "hsl(236, 100%, 87%)";
+
+//   innerHTML added 
+
+a1.innerHTML=""
+a2.innerHTML=""
+a3.innerHTML=""
+
+
+});
